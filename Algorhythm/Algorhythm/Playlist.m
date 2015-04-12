d926cbadc47a1399a7132a638c94b599c4cf0b8a
Yoonsung <lvev9925@naver.com>
2015-04-12 23:07:48 +0900

    (TeamTreeHouse) Create Playlist model class

---
+++ Algorhythm/Algorhythm/Playlist.m
@@ -0,0 +1,59 @@
+//
+//  Playlist.m
+//  Algorhythm
+//
+//  Created by yoon on 2015. 4. 12..
+//  Copyright (c) 2015년 architree. All rights reserved.
+//
+
+#import "Playlist.h"
+#import "MusicLibrary.h"
+
+extern NSString *const kTitle;
+extern NSString *const kDescription;
+extern NSString *const kIcon;
+extern NSString *const kLargeIcon;
+extern NSString *const kBackgroundColor;
+extern NSString *const kArtists;
+
+@implementation Playlist
+
+- (instancetype)initWithIndex: (NSUInteger) index
+{
+    self = [super init];
+    if (self) {
+        
+        MusicLibrary *musicLibrary = [[MusicLibrary alloc] init];
+        NSArray *library = musicLibrary.library;
+        
+        NSDictionary *playlistDictionary = library[index];
+        
+        _playlistTitle = [playlistDictionary objectForKey:kTitle];
+        _playlistDescription = [playlistDictionary objectForKey:kDescription];
+        
+        NSString *iconName = [playlistDictionary objectForKey:kIcon];
+        _playlistIcon = [UIImage imageNamed:iconName];
+        
+        NSString *largeIconName = [playlistDictionary objectForKey:kLargeIcon];
+        _playlistIconLarge = [playlistDictionary objectForKey:largeIconName];
+        
+        _playlistArtists = [NSArray arrayWithArray:[playlistDictionary objectForKey:kArtists]];
+        
+        NSDictionary *colorDictionary = [playlistDictionary objectForKey:kBackgroundColor];
+        
+        _backgroundColor = [self rgbColorFromDictionary:colorDictionary];
+    }
+    return self;
+}
+
+-(UIColor *) rgbColorFromDictionary: (NSDictionary *) colorDictionary {
+    CGFloat red = [[colorDictionary objectForKey:@"red"] doubleValue];
+    CGFloat green = [[colorDictionary objectForKey:@"green"] doubleValue];
+    CGFloat blue = [[colorDictionary objectForKey:@"blue"] doubleValue];
+    CGFloat alpha = [[colorDictionary objectForKey:@"alpha"] doubleValue];
+    
+    return [UIColor colorWithRed:red/255 green:green/255 blue:blue/255 alpha:alpha];
+}
+
+
+@end
