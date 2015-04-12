9bab4ded226215e517deb99ca03d9ced554097ae
Yoonsung <lvev9925@naver.com>
2015-04-12 18:30:29 +0900

    (TeamTreeHouse) Create Second Practice Project

--- Algorhythm/Algorhythm/ViewController.m
+++ Algorhythm/Algorhythm/ViewController.m
@@ -16,12 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.aButton setTitle:@"Press me!" forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonPressed:(id)sender {
+    self.view.backgroundColor = [UIColor orangeColor];
 }
 
 @end
