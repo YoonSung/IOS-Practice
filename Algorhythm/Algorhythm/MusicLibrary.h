1ee11df80f876a4dc59c71e091777bcabbd8b801
Yoonsung <lvev9925@naver.com>
2015-04-12 22:25:37 +0900

    (TeamTreeHouse) Add MusicLibrary model class

---
+++ Algorhythm/Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by yoon on 2015. 4. 12..
+//  Copyright (c) 2015년 architree. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong, nonatomic) NSArray *library;
+
+@end
